ab87a3f7761f69a81c83a10af26bc21ba3fe49af
Paweł Drożański <pawdro@gmail.com>
2013-04-08 21:12:50 +0200

    new Rails app generated by Rails Apps Composer gem

--- Gemfile
+++ Gemfile
@@ -1,15 +1,12 @@
 source 'https://rubygems.org'
-
-gem 'rails'
+gem 'rails', '3.2.12'
 gem 'sqlite3'
-gem 'jquery-rails'
-
 group :assets do
-  gem 'sass-rails'
-  gem 'coffee-rails'
-  gem 'uglifier'
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
 end
-
-group :test, :development do
-  gem 'rspec-rails'
-end
+gem 'jquery-rails'
+gem "haml", ">= 3.1.6"
+gem "haml-rails", ">= 0.3.4", :group => :development
+gem "bootstrap-sass", ">= 2.0.4.0"
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Homework::Application.config.secret_token = 'ccccdd37bc250bf174d6a99a374b6f6bd8edaa0c7bb1a947ad6ad50080932dce8b93c55c98fa3daef95195329706d7861e9f047969fff6f0776fb19e5166c022'
+Homework::Application.config.secret_token = '496f603d9d9b7cc3ceec1962b0767f5776c7c4c5381edbf35881177ddb64c6fec04e658d5dccfd4097874d07526280004e2509abfb8bfeb056710493d95658d3'
