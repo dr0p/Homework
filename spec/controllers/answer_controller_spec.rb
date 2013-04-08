b11d87429ae75bc9f92d39e3d1ea054c013ead8a
Paweł Drożański <pawdro@gmail.com>
2013-04-09 00:46:44 +0200

    answer controller

---
+++ spec/controllers/answer_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe AnswerController do
+  it "can be instantiated" do
+    Answer.new.should be_an_instance_of(Answer)
+  end
+
+  it "can be saved successfully" do
+    Answer.create.should be_persisted
+  end
+
+end
