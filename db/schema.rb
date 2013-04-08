cc0b36e3197d939f7df3ac3557f4b5c554ea0140
Paweł Drożański <pawdro@gmail.com>
2013-04-09 00:50:14 +0200

    answer controller,model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408212941) do
+ActiveRecord::Schema.define(:version => 20130408224734) do
+
+  create_table "answers", :force => true do |t|
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "questions", :force => true do |t|
     t.string   "name"
