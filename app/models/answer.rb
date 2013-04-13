1f64fe8667b4117f9ad9a19b0d92d4175f89063f
Paweł Drożański <pawdro@gmail.com>
2013-04-14 00:15:47 +0200

    answer_spec.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ActiveRecord::Base
   attr_accessible :content
+
+  validates :content, :presence => true
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Answer do
-  it "can be instantiated" do
-    Answer.new.should be_an_instance_of(Answer)
+  it 'is valid content' do
+    expect(Answer.create(content: "Good answear...")).to be_valid
   end
 
-  it "can be saved successfully" do
-    Answer.create.should be_persisted
+  it 'is invalid without content' do
+    expect(Answer.create(content: nil)).not_to be_valid
   end
 end
