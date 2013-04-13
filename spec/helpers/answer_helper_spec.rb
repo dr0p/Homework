fa51208edeffd496f5ac53e7aada58bdb071caa4
Paweł Drożański <pawdro@gmail.com>
2013-04-13 14:11:12 +0200

    more tests

--- spec/helpers/answer_helper_spec.rb
+++ spec/helpers/answer_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe AnswerHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe AnswerHelper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
